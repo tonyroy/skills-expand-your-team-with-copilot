f61f2dc650852c39f92ae866c6aab6a3f02e07c8
tonyroy <zaphodcat@gmail.com>
2025-09-06 18:46:45 +1000

    fix:  make startup work (#15)

--- .devcontainer/installMongoDB.sh
+++ .devcontainer/installMongoDB.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Install MongoDB
-wget -qO - https://www.mongodb.org/static/pgp/server-7.0.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+# Install MongoDB (updated for GPG key verification)
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
